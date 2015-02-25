d333c66dc94df3a246d5697122b01354115fff8f
Joseph Magen <joseph@isratrade.co.il>
2015-02-25 17:39:37 +0200

    more lifecycle environment

--- app/controllers/api/v21/lifecycle_environments_controller.rb
+++ app/controllers/api/v21/lifecycle_environments_controller.rb
@@ -4,13 +4,30 @@ module Api
 
       include Api::Version21
 
+      before_filter :find_organization, :only => [:index, :create, :show, :update, :destroy]
+      before_filter :find_lifecycle_environment, :only => [:show, :update, :destroy]
+
       def index
-        @lifecycle_environments = ::Katello::KTEnvironment.all
+        @lifecycle_environments = ::Katello::KTEnvironment.readable.where(:organization_id => @organization.id)
         render :json => @lifecycle_environments, :each_serializer => LifecycleEnvironmentSerializer
       end
 
       def show
- #       render :json => @environment, :serializer => EnvironmentSerializer
+        render :json => @lifecycle_environment, :serializer => LifecycleEnvironmentSerializer
+      end
+
+      private
+
+      def find_organization
+        @organization = ::Organization.find_by_id(params[:organization_id])
+        return @organization if @organization
+        return :json => {:error => "Couldn't find organization" }
+      end
+
+      def find_lifecycle_environment
+        @lifecycle_environment = ::Katello::KTEnvironment.find_by_id(params[:id])
+        return @lifecycle_environment if @lifecycle_environment
+        return :json => {:error => "Couldn't find lifecycle environment" }
       end
 
     end
