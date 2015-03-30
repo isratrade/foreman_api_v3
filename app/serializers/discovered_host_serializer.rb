44a6c4707cbaa9ad4dbf5a53b8297a3e313eec5b
Joseph Magen <jmagen@redhat.com>
2015-03-30 23:29:01 +0300

    remove cpus attribute from DiscoveredHostSerializer

--- app/serializers/discovered_host_serializer.rb
+++ app/serializers/discovered_host_serializer.rb
@@ -3,7 +3,7 @@ class DiscoveredHostSerializer < ActiveModel::Serializer
   attributes :id, :name,
              :ip, :last_report, :mac,
              :subnet_id, :subnet_name,
-             :memory, :disk_count, :disks_size, :cpus,
+             :memory, :disk_count, :disks_size,
              :organization_id, :organization_name, :location_id, :location_name,
              :created_at, :updated_at
 end
