8cca22b400f8682fe0b447f5aff1e4dde7795f83
Joseph Magen <jmagen@redhat.com>
2015-04-08 16:40:06 +0300

    add cpus to discovered host response

--- app/serializers/discovered_host_serializer.rb
+++ app/serializers/discovered_host_serializer.rb
@@ -3,7 +3,11 @@ class DiscoveredHostSerializer < ActiveModel::Serializer
   attributes :id, :name,
              :ip, :last_report, :mac,
              :subnet_id, :subnet_name,
-             :memory, :disk_count, :disks_size,
+             :memory, :disk_count, :disks_size, :cpus,
              :organization_id, :organization_name, :location_id, :location_name,
              :created_at, :updated_at
+
+  def cpus
+        object.cpu_count
+  end
 end
