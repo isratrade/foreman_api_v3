e607548bc7cc539ba2abdd1d9bf8fb98df308da8
Joseph Magen <jmagen@redhat.com>
2015-05-19 20:37:31 +0300

    added prior_id attribute to lifecycle_environment_serializer

--- app/serializers/lifecycle_environment_serializer.rb
+++ app/serializers/lifecycle_environment_serializer.rb
@@ -1,8 +1,11 @@
 class LifecycleEnvironmentSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :name, :label, :description, :library, :prior,
+  attributes :id, :name, :label, :description, :library, :prior_id, :prior,
              :created_at, :updated_at
 
+  def prior_id
+    prior.try(:id)
+  end
   #no child node for counts:{} and permissions: {} which are in v2
 end
 
