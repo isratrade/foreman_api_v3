be4770eaa18760ea44ac5c08e024a1887c1ceff4
Joseph Magen <jmagen@redhat.com>
2015-07-27 17:51:06 +0300

    add scoped_search to foreman_tasks controller

--- app/controllers/api/v21/foreman_tasks_controller.rb
+++ app/controllers/api/v21/foreman_tasks_controller.rb
@@ -5,7 +5,21 @@ module Api
       include Api::Version21
 
       def index
-        @foreman_tasks = ForemanTasks::Task.limit(30).order('id desc') # TODO - temp, change in future
+        @foreman_tasks = ForemanTasks::Task.search_for(params[:search]).select('DISTINCT foreman_tasks_tasks.*')
+
+        ordering_params =  {
+                             sort_by: params[:sort_by] || 'started_at',
+                             sort_order: params[:sort_order] || 'DESC'
+                           }
+        @foreman_tasks = ordering_scope(@foreman_tasks, ordering_params)
+
+
+        pagination_params = {
+                              page: params[:page] || 1,
+                              per_page: params[:per_page] || 20
+                            }
+        @foreman_tasks = pagination_scope(@foreman_tasks, pagination_params)
+
         render :json => @foreman_tasks, :each_serializer => ForemanTaskSerializer
       end
 
