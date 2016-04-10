74edf67d02061baae722add72dd052d11ee85309
Joseph Magen <jmagen@redhat.com>
2016-04-10 13:34:30 +0300

    change "after" hoook to "before", this caused failure

--- lib/foretello_api_v21/engine.rb
+++ lib/foretello_api_v21/engine.rb
@@ -3,12 +3,13 @@ require 'active_model_serializers'
 module ForetelloApiV21
   class Engine < ::Rails::Engine
 
+    # support pre-4 Rails versions
     config.autoload_paths += Dir["#{config.root}/app/controllers/concerns"]
     config.autoload_paths += Dir["#{config.root}/app/serializers"]
 
-    initializer 'foretello_api_v21.register_plugin', :after => :finisher_hook do |app|
+    initializer 'foretello_api_v21.register_plugin', :before => :finisher_hook do |app|
       Foreman::Plugin.register :foretello_api_v21 do
-        requires_foreman '>= 1.7'
+        requires_foreman '>= 1.11.0'
 
         security_block :foretello_api_v21 do
           permission :rename_discovered_hosts, {
