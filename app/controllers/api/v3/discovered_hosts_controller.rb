95bac490cef1d2f1f08af66619e5cf22a5c5458f
Joseph Magen <jmagen@redhat.com>
2016-05-15 15:37:11 +0300

    deleted serializers

--- app/controllers/api/v3/discovered_hosts_controller.rb
+++
@@ -1,8 +0,0 @@
-module Api
-  module V3
-    #TODO CHANGE
-    class DiscoveredHostsController < V3::BaseController
-
-    end
-  end
-end
--- app/controllers/api/v3/foreman_tasks_controller.rb
+++
@@ -1,8 +0,0 @@
-module Api
-  module V3
-    #TODO CHANGE
-    class ForemanTasksController < V3::BaseController
-
-    end
-  end
-end
